769ded0312427ebd454020443cd378814951247b
Brozda <daniel.brozik@gmail.com>
2025-07-06 12:18:59 +0200

    feat: Add enpoints implementation

---
+++ ShoppingList.BrozDa/ShoppingList.BrozDa.Server/Endpoints/ShoppingListEndpoints.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShoppingList.BrozDa.Server.Data;
+using ShoppingList.BrozDa.Server.Models;
+
+namespace ShoppingList.BrozDa.Server.Endpoints
+{
+    public static class ShoppingListEndpoints
+    {
+        public static IEndpointRouteBuilder AddShoppingListEndpoints(this IEndpointRouteBuilder builder) 
+        {
+            var group = builder.MapGroup("/shopping-list");
+
+            //C ------------------------------------------------------------------------
+            group.MapPost("/", async (AddShoppingListItem newItem, ShoppingListContext context) =>
+            {
+                var result = await context.ShoppingList.AddAsync(new ShoppingListItem() {Name= newItem.Name,IsPickedUp = newItem.IsPickedUp});
+                
+                await context.SaveChangesAsync();
+
+                return result is not null
+                ? Results.Created($"/shopping-list/{result.Entity.Id}", result)
+                : Results.BadRequest();
+            });
+            //R ------------------------------------------------------------------------
+            group.MapGet("/", async (ShoppingListContext context) => 
+            {
+                var result = await context.ShoppingList.ToListAsync();
+
+                return Results.Ok(result);
+
+            });
+
+            group.MapGet("/{id:Guid}", async (Guid id, ShoppingListContext context) =>
+            {
+                var result = await context.ShoppingList.FindAsync(id);
+
+                return result is not null
+                    ? Results.Ok(result)
+                    : Results.NotFound();
+            });
+
+            //U ------------------------------------------------------------------------
+
+            group.MapPut("/{id:Guid}", async (ShoppingListItem updatedItem, ShoppingListContext context) =>
+            {
+                var result = await context.ShoppingList.FindAsync(updatedItem.Id);
+
+                if (result is null) { return Results.NotFound(); }
+
+                result.Name = updatedItem.Name;
+                result.IsPickedUp = updatedItem.IsPickedUp;
+
+                await context.SaveChangesAsync();
+
+                return Results.Ok();
+            });
+
+            //D ------------------------------------------------------------------------
+            group.MapDelete("/{id:Guid}", async (Guid id, ShoppingListContext context) =>
+            {
+                var result = await context.ShoppingList.Where(x =>  x.Id == id).ExecuteDeleteAsync();
+
+                return result != 0
+                    ? Results.Ok() 
+                    : Results.NotFound();
+            });
+
+            return builder;
+        
+        }
+    }
+}
---
+++ ShoppingList.BrozDa/ShoppingList.BrozDa.Server/Models/AddShoppingListItem.cs
@@ -0,0 +1,8 @@
+﻿namespace ShoppingList.BrozDa.Server.Models
+{
+    public class AddShoppingListItem
+    {
+        public string Name { get; set; } = null!;
+        public bool IsPickedUp { get; set; }
+    }
+}
